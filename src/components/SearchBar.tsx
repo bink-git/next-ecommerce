6f3e2ca4709b654f2778e7047671b1b092119aba
bink-git <bink@email.ua>
2024-06-01 11:54:44 +0200

    add navbar

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+'use client';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const SearchBar = () => {
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get('name');
+
+    if (name) {
+      router.push(`/list?name=${name}`);
+    }
+  };
+
+  return (
+    <form
+      className="flex items-center justify-between gap-4 bg-slate-100 p-2 rounded-md flex-1"
+      onSubmit={handleSearch}
+    >
+      <input
+        type="text"
+        name="name"
+        placeholder="Search"
+        className="flex-1 bg-transparent outline-none"
+      />
+      <button className="cursor-pointer">
+        <Image src="/search.png" alt="search" width={24} height={24} />
+      </button>
+    </form>
+  );
+};
+
+export default SearchBar;
