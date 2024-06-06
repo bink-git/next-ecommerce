9e3b044f950498bfda61f6f49e2ce4ede7f77aab
bink-git <bink@email.ua>
2024-06-06 21:37:29 +0200

    add categories

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,269 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+const CategoryList = () => {
+  return (
+    <div className="px-4 overflow-x-scroll scrollbar-hide">
+      <div className="flex gap-4 md:gap-8">
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+        <Link
+          href="/list?cat=test"
+          className="flex-shrink-0 w-full sm:w-1/2 lg:w-1/4 xl:w-1/6"
+        >
+          <div className="relative bg-slate-100 w-full h-96">
+            <Image
+              src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+              alt=""
+              fill
+              sizes="20vw"
+              className="object-cover"
+            />
+          </div>
+          <h1 className="mt-8 font-light text-xl tracking-wide">
+            Category name
+          </h1>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,127 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+const ProductList = () => {
+  return (
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white ">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white ">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white ">
+          Add to cart
+        </button>
+      </Link>
+      <Link
+        href="/test"
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+      >
+        <div className="relative w-full h-80">
+          <Image
+            src="https://images.unsplash.com/photo-1716339140080-be256d3270ce?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+          />
+          <Image
+            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            alt="product image"
+            fill
+            sizes="25vw"
+            className="absolute object-cover rounded-md"
+          />
+        </div>
+        <div className="flex justify-between">
+          <span className="font-medium">Product name</span>
+          <span className="font-semibold">$49</span>
+        </div>
+        <div className="text-sm text-gray-500">My description</div>
+        <button className="rounded-2xl w-max ring-1 ring-lama text-lama py-2 px-4 text-xs hover:bg-lama hover:text-white ">
+          Add to cart
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default ProductList;
