f1015a502e663941f8501b9cb70ffd593256ef68
bink-git <bink@email.ua>
2024-06-09 16:37:45 +0200

    add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,17 @@ const HomePage = () => {
         <h1 className="text-2xl">Featured products</h1>
         <ProductList />
       </div>
+
       <div className="mt-24 ">
         <h1 className="text-2xl px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 mb-12">
           Categories
         </h1>
         <CategoryList />
+      </div>
+
+      <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+        <h1 className="text-2xl">New products</h1>
+        <ProductList />
       </div>
     </div>
   );
